c7ffb3f8a4683ce03480432475a55f99c143be35
conorw <conorw@gmail.com>
2017-06-20 11:38:57 +0100

    login with google

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,18 +3,37 @@ import { NavController } from 'ionic-angular';
 
 import { MainPage } from '../main/main';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        return;
+      }
+      this.navCtrl.push(MainPage, 1);
+    });
   }
 
-  Login(){
-    this.navCtrl.push(MainPage,1);
+  signInWithGoogle() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(res => {
+        console.log(res);
+        this.navCtrl.push(MainPage, 1);
+      });
+  }
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+  Login() {
+    this.navCtrl.push(MainPage, 1);
   }
 
 }
